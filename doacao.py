5e044e37e37bc48f33caa40f0722f834e9e9d9ff
Tiago370 <tiagoamral23@gmail.com>
2026-06-24 15:42:51 -0300

    ADD chaves PIX pelo .env

--- doacao.py
+++ doacao.py
@@ -1,9 +1,15 @@
 from flask import Blueprint, render_template, redirect, request, session
 from utils import db, calcular_total
+from dotenv import load_dotenv
+import os
+
 doacao_bp = Blueprint("doacao", __name__)
 
 @doacao_bp.route("/doacao",methods=["GET","POST"])
 @doacao_bp.route("/doacao/",methods=["GET","POST"])
 def doacao():
+    load_dotenv()
+    PIX_KEY_ANY = os.getenv("PIX_KEY_ANY")
+    PIX_KEY_1_REAL = os.getenv("PIX_KEY_1_REAL")
+    return render_template("doacao.html",PIX_KEY_ANY=PIX_KEY_ANY,PIX_KEY_1_REAL=PIX_KEY_1_REAL)
 
-    return render_template("doacao.html")
