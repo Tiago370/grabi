e9c722dbfb5144b2c947b4e0eda2ad52fd8ce456
Tiago370 <tiagoamral23@gmail.com>
2026-06-27 00:59:56 -0300

    ADD rota que mostra preços de produtos do estbelecimento

--- estabelecimento.py
+++ estabelecimento.py
@@ -1,6 +1,7 @@
-from flask import Blueprint, render_template, redirect, request, session
-from utils import db, calcular_total
-from banco import Estabelecimento
+from datetime import datetime, timedelta
+from flask import Blueprint, render_template, redirect, request, session, abort
+from utils import db, calcular_total, render_page
+from banco import Estabelecimento, Preco
 estabelecimento_bp = Blueprint("estabelecimento", __name__)
 
 @estabelecimento_bp.route("/estabelecimento",methods=["GET","POST"])
@@ -18,3 +19,17 @@ def estabelecimento():
             Estabelecimento(cnpj=cnpj,nome=nome,endereco=endereco,latitude=latitude,longitude=longitude).save()
             return redirect("/")
     return render_template("estabelecimento.html")
+
+@estabelecimento_bp.route("/estabelecimento/<cnpj>/",methods=["GET"])
+def mostrar_estabelecimento(cnpj):
+    estabelecimento = Estabelecimento.get(cnpj)
+    print(estabelecimento)
+
+#    if not estabelecimento:
+#        abort(404)
+
+    limite_recente = datetime.utcnow() - timedelta(days=7)
+
+    precos = Preco.get_all()
+
+    return render_page("mostrar_estabelecimento.html", estabelecimento=estabelecimento, precos=precos)
